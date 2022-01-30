5be80ac1b5cdfe3fbe67eb0b9392fca0cd34542b
PauSanCli <pauglu777@gmail.com>
2022-01-30 19:06:38 +0100

    routes + components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainComponent } from './main/main.component';
+import {IndexComponent } from './index/index.component';
+import {ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+
+    { path: '', redirectTo: 'main', pathMatch: 'full'},
+    { path: 'main', component: MainComponent},
+    { path: 'index', component: IndexComponent},
+    { path: 'contact', component: ContactComponent},
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['../styles/front-page.css', 
+              '../styles/estilos-button.css', 
+              '../styles/estilos.css']
+
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
