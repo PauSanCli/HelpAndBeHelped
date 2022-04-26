ec0a3e3c4bb37cbcf1f59d5acd2aa8acbf126dc3
PauSanCli <pauglu777@gmail.com>
2022-04-26 15:43:36 +0200

    creados componentes iniciales

--- help-frontend/help-frontend/src/app/app-routing.module.ts
+++ help-frontend/help-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseComponent } from './Includes/base/base.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'base', component: BaseComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- help-frontend/help-frontend/src/app/app.module.ts
+++ help-frontend/help-frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BaseComponent } from './Includes/base/base.component';
+import { FooterComponent } from './Includes/footer/footer.component';
+import { HeaderComponent } from './Includes/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
